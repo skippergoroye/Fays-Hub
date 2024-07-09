be648c8ee72aeceb8cd9ef06944a6cc770804c7e
skippergoroye <oluwatosinadegoroye21@gmail.com>
2024-07-09 21:28:51 +0100

    feat: color added to the button

--- src/app/paynow/page.tsx
+++ src/app/paynow/page.tsx
@@ -9,8 +9,8 @@ export default function PayNow() {
   return (
     <section className="flex justify-center items-center min-h-screen">
       <div>
-        <div className="mb-4">
-          <p onClick={() => router.push("/")} className="text-center cursor-pointer">Back To Home</p>
+        <div className="mb-4 flex items-center justify-center">
+          <button onClick={() => router.push("/")} className="text-center cursor-pointer bg-green-500 w-40 p-2 rounded-lg text-white hover:bg-green-800">Back To Home</button>
         </div>
         <div className="p-4 sm:p-6 w-full sm:w-[600px] h-auto sm:h-[400px] bg-gray-100 border gap-4 sm:gap-10 flex flex-col items-center justify-center rounded-lg shadow-2xl">
           <Image
